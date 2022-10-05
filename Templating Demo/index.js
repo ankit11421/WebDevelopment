9aefefa817d10c9e00c3cad4725373a930ce5386
Ankit Chaudhary <ankit11421@gmail.com>
2022-10-05 12:37:49 +0530

    ejs

--- Templating Demo/index.js
+++ Templating Demo/index.js
@@ -1,11 +1,25 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+
 
 app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, '/views'));
+
 
 app.get("/", (req, res) => {
     res.render("home");
 });
+
+app.get('/rand', (req, res) => {
+    const num = Math.floor(Math.random() * 10) + 1;
+    res.render('random', {rand:num});
+});
+
+app.get('/r/:subreddit', (req, res) => {
+    const { subreddit } = req.params;
+    res.render('subreddit', {subreddit});
+})
 
 app.listen(8080, () => {
     console.log("Listening on port 8080");
